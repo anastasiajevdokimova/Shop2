cfeaa29ee2b0becacf4867d0ca2c1dadad51674f
Anastasia Jevdokimova <a.jevdokimova98@gmail.com>
2021-11-08 14:12:31 +0200

    ProductAndDbContext

---
+++ Shop2/Shop2/Models/Product/ProductListViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop2.Models.Product
+{
+    public class ProductListViewModel
+    {
+        public  Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Ammount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set }
+
+    }
+}
