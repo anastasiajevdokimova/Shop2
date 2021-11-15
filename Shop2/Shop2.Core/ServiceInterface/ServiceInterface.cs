c564c39deccd5925759aba6bf7bb5544fd57fd65
Anastasia Jevdokimova <a.jevdokimova98@gmail.com>
2021-11-15 14:15:25 +0200

    addbtn

---
+++ Shop2/Shop2.Core/ServiceInterface/ServiceInterface.cs
@@ -0,0 +1,11 @@
+﻿using Shop2.Core.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace Shop2.Core.ServiceInterface
+{
+    public interface ServiceInterface
+    {
+        Task<Product> Delete(Guid id);
+    }
+}
