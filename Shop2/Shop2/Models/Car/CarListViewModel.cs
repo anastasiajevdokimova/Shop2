357ccf1ad2a6bb5789a2b6edfa9831a54a4f6b45
anastasiajevdokimova <a.jevdokimova98@gmail.com>
2021-12-28 14:35:56 +0200

    CarIndexView

---
+++ Shop2/Shop2/Models/Car/CarListViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop2.Models.Car
+{
+    public class CarListViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Bodytype { get; set; }
+        public int Year { get; set; }
+        public double Power { get; set; }
+        public double Mileage { get; set; }
+        public string Fuel { get; set; }
+        public string Transmission { get; set; }
+        public string Drivetrain { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
