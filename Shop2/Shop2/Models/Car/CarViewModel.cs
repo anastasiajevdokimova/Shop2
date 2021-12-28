682699ac8d20c36be355ef26360b1d5087705291
anastasiajevdokimova <a.jevdokimova98@gmail.com>
2021-12-28 14:43:27 +0200

    CarView

--- Shop2/Shop2/Models/Car/CarViewModel.cs
+++ Shop2/Shop2/Models/Car/CarViewModel.cs
@@ -7,5 +7,18 @@ namespace Shop2.Models.Car
 {
     public class CarViewModel
     {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Bodytype { get; set; }
+        public int Year { get; set; }
+        public double Power { get; set; }
+        public double Mileage { get; set; }
+        public string Fuel { get; set; }
+        public string Transmission { get; set; }
+        public string Drivetrain { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
     }
 }
