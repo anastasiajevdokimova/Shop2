61182ee523691a491f7135a008ceb2add27c2b88
anastasiajevdokimova <a.jevdokimova98@gmail.com>
2021-12-29 11:34:27 +0200

    AddPhoto

---
+++ Shop2/Shop2.Core/Domain/CarExistingFilePath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop2.Core.Domain
+{
+    public class CarExistingFilePath
+    {
+        public Guid Id { get; set; }
+        public string FilePath { get; set; }
+        public Guid? CarId { get; set; }
+    }
+}
