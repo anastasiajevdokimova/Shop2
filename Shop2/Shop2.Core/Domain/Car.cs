565d40200f88e0ed984f8e1ffd39cf63a2fe46ff
anastasiajevdokimova <a.jevdokimova98@gmail.com>
2022-01-17 10:15:35 +0200

    Update

--- Shop2/Shop2.Core/Domain/Car.cs
+++ Shop2/Shop2.Core/Domain/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Shop2.Core.Domain
 {
     public class Car
     {
+        [Key]
         public Guid? Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
