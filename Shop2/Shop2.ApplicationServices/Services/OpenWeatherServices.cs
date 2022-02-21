77a7cb94e0b4db14da897d491ee34cc91c98ff3b
anastasiajevdokimova <a.jevdokimova98@gmail.com>
2022-02-21 17:20:07 +0200

    OpenWeatherServices

--- Shop2/Shop2.ApplicationServices/Services/OpenWeatherServices.cs
+++ Shop2/Shop2.ApplicationServices/Services/OpenWeatherServices.cs
@@ -1,8 +1,10 @@
-﻿using Shop2.Core.Dtos.OpenWeather;
+﻿using Nancy.Json;
+using Shop2.Core.Dtos.OpenWeather;
 using Shop2.Core.ServiceInterface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +17,42 @@ namespace Shop2.ApplicationServices.Services
             string apikey = "d7e4c8388840f164d4bd9f7804aec898";
             var url = $"http://api.openweathermap.org/data/2.5/weather?q=London&limit=5&appid={apikey}";
 
+            using(WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
 
+                OpenWeatherDto weatherInfo = new JavaScriptSerializer().Deserialize<OpenWeatherDto>(json);
+
+                dto.name = weatherInfo.coord.name;
+                dto.main = weatherInfo.weather[0].main;
+                dto.description = weatherInfo.weather[0].description;
+                dto.icon = weatherInfo.weather[0].icon;
+
+                dto.temp = weatherInfo.main.temp;
+                dto.feels_like = weatherInfo.main.feels_like;
+                dto.temp_min = weatherInfo.main.temp_min;
+                dto.temp_max = weatherInfo.main.temp_max;
+                dto.pressure = weatherInfo.main.pressure;
+                dto.humidity = weatherInfo.main.humidity;
+                dto.visibility = weatherInfo.visibility;
+
+                dto.speed = weatherInfo.wind.speed;
+                dto.deg = weatherInfo.wind.deg;
+
+                dto.all = weatherInfo.clouds.all;
+
+                dto.dt = weatherInfo.dt;
+
+                dto.type = weatherInfo.sys.type;
+                dto.id = weatherInfo.sys.id;
+                dto.message = weatherInfo.sys.message;
+                dto.country = weatherInfo.sys.country;
+                dto.sunrise = weatherInfo.sys.sunrise;
+                dto.sunset = weatherInfo.sys.sunset;
+                dto.timezone = weatherInfo.timezone;
+
+                var jsonString = new JavaScriptSerializer().Serialize(dto);
+            }
             return dto;
         } 
     }
--- Shop2/Shop2.Core/ServiceInterface/IOpenWeatherServices.cs
+++ Shop2/Shop2.Core/ServiceInterface/IOpenWeatherServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shop2.Core.Dtos.OpenWeather;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Shop2.Core.ServiceInterface
 {
     public interface IOpenWeatherServices
     {
+        Task<OpenWeatherResultDto> WeatherDetail(OpenWeatherResultDto dto)
     }
 }
